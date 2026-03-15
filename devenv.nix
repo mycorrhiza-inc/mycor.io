1e0ae57e1f9b4f1a76c5010a1e02a3b3cdac0881
Bradley Venner <brad@Maries-Mac-mini.local>
2026-03-15 12:21:14 -0600

    cabin week work

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,17 @@
 { pkgs, ... }:
 {
-  languages.javascript = {
+  env.OPENROUTER_MODEL = "google/gemini-3.1-pro-preview";
+
+  packages = [
+    pkgs.quarto
+  ];
+
+  languages.r = {
+    enable = true;
+    package = pkgs.R;
+  };
+
+  languages.julia = {
     enable = true;
     package = pkgs.nodejs_22;
     corepack.enable = true;
